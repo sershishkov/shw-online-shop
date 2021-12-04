73b976ef95ca48164349e2eeb3c8480c77fbcf43
sershishkov <ser.shishkov@gmail.com>
2021-12-04 15:08:17 +0200

    567

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 const checkAuthStatusMiddleware = require('./middlewares/check-auth');
+const checkRoutesMiddleware = require('./middlewares/protect-routes');
 
 const authRoutes = require('./routes/auth.routes');
 const productsRoutes = require('./routes/products.routes');
@@ -33,6 +34,7 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
+app.use(checkRoutesMiddleware);
 app.use('/admin', adminRoutes);
 
 app.use(errorHandlerMiddleware);
